eb911100ba21f1c397f534dfd3e19884f84244c5
Ahsan Shakeel <ahsanshakeel1994@gmail.com>
2020-07-19 22:35:21 +0500

    Practising Web Animation

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import useWebAnimations from "@wellyshen/use-web-animations";
 
 function App() {
+  const { ref, playState, getAnimation } = useWebAnimations({
+    keyframes: [
+      { background: "red", transform: "translateX(0vw)" },
+      { background: "blue" },
+      { background: "green", transform: "translateX(90vw)" },
+    ],
+    timing: {
+      duration: 5000,
+      iterations: Infinity,
+      easing: "ease-in-out",
+      direction: "alternate",
+    },
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 style={{ textAlign: "center" }}>PlayState: {playState}</h1>
+      <div
+        ref={ref}
+        style={{
+          backgroundColor: "red",
+          width: "100px",
+          height: "100px",
+          borderRadius: "100%",
+        }}
+      ></div>
     </div>
   );
 }
