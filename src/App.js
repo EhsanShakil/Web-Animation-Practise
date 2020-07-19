6791f675f8d31ca562d2a81b2f4effd7ca04bc1d
Ahsan Shakeel <ahsanshakeel1994@gmail.com>
2020-07-19 22:53:06 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
       { background: "green", transform: "translateX(90vw)" },
     ],
     timing: {
-      duration: 5000,
+      duration: 6000,
       iterations: Infinity,
       easing: "ease-in-out",
       direction: "alternate",
@@ -28,6 +28,73 @@ function App() {
           borderRadius: "100%",
         }}
       ></div>
+      <br />
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <button
+          onClick={() => getAnimation().play()}
+          style={{
+            fontSize: "20px",
+            fontWeight: "bold",
+            backgroundColor: "white",
+            outline: "none",
+            width: "100px",
+            height: "100px",
+            border: "5px solid black",
+            borderRadius: "100%",
+          }}
+        >
+          Play
+        </button>
+        <button
+          onClick={() => getAnimation().pause()}
+          style={{
+            fontSize: "20px",
+            fontWeight: "bold",
+            backgroundColor: "white",
+            outline: "none",
+            width: "100px",
+            height: "100px",
+            border: "5px solid black",
+            borderRadius: "100%",
+          }}
+        >
+          Pause
+        </button>
+        <button
+          onClick={() =>
+            getAnimation().updatePlaybackRate(getAnimation().playbackRate * 1.2)
+          }
+          style={{
+            fontSize: "20px",
+            fontWeight: "bold",
+            backgroundColor: "white",
+            outline: "none",
+            width: "100px",
+            height: "100px",
+            border: "5px solid black",
+            borderRadius: "100%",
+          }}
+        >
+          High
+        </button>
+        <button
+          onClick={() =>
+            getAnimation().updatePlaybackRate(getAnimation().playbackRate * 0.8)
+          }
+          style={{
+            fontSize: "20px",
+            fontWeight: "bold",
+            backgroundColor: "white",
+            outline: "none",
+            width: "100px",
+            height: "100px",
+            border: "5px solid black",
+            borderRadius: "100%",
+          }}
+        >
+          Low
+        </button>
+      </div>
     </div>
   );
 }
